41e1814d5f00ca1f60d94ad4e1718acfc03c4417
Robert Hansson <robert.hansson@xenit.se>
2020-09-18 16:16:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>This site is under maintenance</h1>
+        <h2>We’re preparing to serve you better.</h2>
+      </div>
+      <img className="cable" src="cable.svg" alt="cable" />
+      <img className="mobile-cable" src="mobile-cable.svg" alt="cable" />
+      <img className="xenit" src="Xenit_logo_Vit.png" alt="xenit_logo" />
+    </div>
+  );
+}
+
+export default App;
